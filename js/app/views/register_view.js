1d8a226df1fcf44fcfb32cfb30e04c134bb3c0e9
DavidToca <david.toca@globant.com>
2014-10-15 11:58:32 -0500

    Add length restriction for password/

--- js/app/views/register_view.js
+++ js/app/views/register_view.js
@@ -11,6 +11,7 @@ app.RegisterView = Backbone.View.extend({
 		"click #register_button"	: "register",
 		'keyup .register_input.validate-text'  	: "validateText",
 		'blur .register_input.validate-text'  	: "validateText",
+		'blur .register_input.validate-length'  : "validateLength",
 	},
 
 	initialize: function () {
@@ -64,6 +65,25 @@ app.RegisterView = Backbone.View.extend({
 		this.validateButton();
 
 	},
+	validateLength: function(event){
+		var elem = $('#' + event.target.id);
+		var elem_container = $('#' + event.target.id + "_container");
+		var elem_help_text = $('#' + event.target.id + "_help");
+
+		var error = this.validateElementLength(elem);
+
+		if(error){
+           elem_container.addClass("has-error");
+           elem_help_text.text(error);
+		}
+		else{
+		    elem_container.removeClass("has-error");
+			elem_help_text.text("");
+		}
+
+		this.validateButton();
+
+	},
 	validateElementText: function(element){
 
 		var value = element.val();
@@ -73,6 +93,18 @@ app.RegisterView = Backbone.View.extend({
         }
         else if(!this.isAlfaNum(value)){
         	return "Has invalid characters";
+        }
+
+        return false;
+	},	validateElementLength: function(element){
+
+		var value = element.val();
+
+        if (value === "") {
+        	return "Can\'t be empty";
+        }
+        else if(value.length < 7){
+        	return "Has invalid length, 7 as minimum";
         }
 
         return false;
@@ -88,8 +120,9 @@ app.RegisterView = Backbone.View.extend({
 		var user_error = this.validateElementText(user);
 		var username_error = this.validateElementText(username);
 		var password_error = this.validateElementText(password);
+		var password_length_error = this.validateElementLength(password);
 
-		if(username_error || password_error){
+		if(username_error || password_error ||password_length_error || user_error){
 			submit_button.attr("disabled", "disabled");
 		}
 		else{
