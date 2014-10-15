94ae5cdac1ffc016958a0046fa08259c604d86a2
DavidToca <david.toca@globant.com>
2014-10-15 11:53:01 -0500

    Add Text Validations.

--- js/app/views/login_view.js
+++ js/app/views/login_view.js
@@ -5,14 +5,70 @@ app.LoginView = Backbone.View.extend({
 
 	// template: _.template($('#template_comic_list').html()),
 
+	isAlfaNum: function(input){
+		return input.match(/^\w+$/);
+	},
+
 	events: {
-		"click #login_button": "login"
+		"click #login_button"				: "login",
+		'keyup .login_input.validate-text'  : "validateText",
+		'blur .login_input.validate-text'  : "validateText",
 	},
 
 	initialize: function () {
 
 	},
 
+	validateText: function(event){
+		var elem = $('#' + event.target.id);
+		var elem_container = $('#' + event.target.id + "_container");
+		var elem_help_text = $('#' + event.target.id + "_help");
+
+		var error = this.validateElementText(elem);
+
+		if(error){
+           elem_container.addClass("has-error");
+           elem_help_text.text(error);
+		}
+		else{
+		    elem_container.removeClass("has-error");
+			elem_help_text.text("");
+		}
+
+		this.validateButton();
+
+	},
+	validateElementText: function(element){
+
+		var value = element.val();
+
+        if (value === "") {
+        	return "Can\'t be empty";
+        }
+        else if(!this.isAlfaNum(value)){
+        	return "Has invalid characters";
+        }
+
+        return false;
+	},
+
+	validateButton: function(){
+		var submit_button = $('#login_button');
+
+		var username = $('#form_username');
+		var password = $('#form_password');
+
+		var username_error = this.validateElementText(username);
+		var password_error = this.validateElementText(password);
+
+		if(username_error || password_error){
+			submit_button.attr("disabled", "disabled");
+		}
+		else{
+			submit_button.removeAttr("disabled");
+		}
+
+	},
 	login: function () {
 		var username = $('#form_username').val();
 		var password = $('#form_password').val();
@@ -21,7 +77,7 @@ app.LoginView = Backbone.View.extend({
 			window.location.hash = '';
 		}
 		else{
-			this.render({error: true});
+			this.render({error: 'User or password doesn\'t match.'});
 			return false;
 		}
 	},
--- js/app/views/register_view.js
+++ js/app/views/register_view.js
@@ -4,11 +4,13 @@ app.RegisterView = Backbone.View.extend({
 	el: '#main_content',
 
 	// template: _.template($('#template_comic_list').html()),
-
+	isAlfaNum: function(input){
+		return input.match(/^\w+$/);
+	},
 	events: {
-		"click #register_button": "register",
-		'blur .alfanumeric_field' 		: "alfaNumberValidation",
-		'blur .password' 				: "passwordValidation",
+		"click #register_button"	: "register",
+		'keyup .register_input.validate-text'  	: "validateText",
+		'blur .register_input.validate-text'  	: "validateText",
 	},
 
 	initialize: function () {
@@ -43,6 +45,58 @@ app.RegisterView = Backbone.View.extend({
 
 	},
 
+	validateText: function(event){
+		var elem = $('#' + event.target.id);
+		var elem_container = $('#' + event.target.id + "_container");
+		var elem_help_text = $('#' + event.target.id + "_help");
+
+		var error = this.validateElementText(elem);
+
+		if(error){
+           elem_container.addClass("has-error");
+           elem_help_text.text(error);
+		}
+		else{
+		    elem_container.removeClass("has-error");
+			elem_help_text.text("");
+		}
+
+		this.validateButton();
+
+	},
+	validateElementText: function(element){
+
+		var value = element.val();
+
+        if (value === "") {
+        	return "Can\'t be empty";
+        }
+        else if(!this.isAlfaNum(value)){
+        	return "Has invalid characters";
+        }
+
+        return false;
+	},
+
+	validateButton: function(){
+		var submit_button = $('#register_button');
+
+		var user = $('#register_name');
+		var username = $('#register_username');
+		var password = $('#register_password');
+
+		var user_error = this.validateElementText(user);
+		var username_error = this.validateElementText(username);
+		var password_error = this.validateElementText(password);
+
+		if(username_error || password_error){
+			submit_button.attr("disabled", "disabled");
+		}
+		else{
+			submit_button.removeAttr("disabled");
+		}
+
+	},
 	render: function (errors){
 		var that = this;
 		// I'd rather read the template and then rendered
